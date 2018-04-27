406d4aad51a5bf50993fa70e2ba0f39359157fa4
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-04-27 12:15:32 +0200

    Fix ZIP Path

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -7,7 +7,7 @@ echo "ls build/distributions"
 ls build/distributions
 
 aws lambda update-function-code \
---zip-file=fileb://build/distributions/twitter-1.0-SNAPSHOT.zip \
+--zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
 --function-name=TweetsCounter
 
