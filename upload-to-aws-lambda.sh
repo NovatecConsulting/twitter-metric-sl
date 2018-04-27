1314581165ad76ae5c5104e5fcdfd5c0a37bf738
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-04-27 11:19:33 +0200

    Try to show pwd and envirmoment

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+ls
+tree
+pwd
+
 aws lambda update-function-code \
---zip-file=fileb:///build/distributions/twitter-1.0-SNAPSHOT.zip \
+--zip-file=fileb://build/distributions/twitter-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
 --function-name=TweetsCounter
+
