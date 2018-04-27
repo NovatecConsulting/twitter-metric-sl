9136c32089c5c405c4eaa73d8d836410702c2681
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-04-27 12:24:04 +0200

    Add missing AWS Credentials

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-echo "ls build"
-ls build
-
-echo "ls build/distributions"
-ls build/distributions
-
 aws lambda update-function-code \
 --zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
