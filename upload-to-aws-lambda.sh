c5eabab15d6201b3dc959f76a5e182f9b151d97e
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-05-11 11:02:05 +0200

    Try the strange syntax from the docu

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws lambda update-function-code \
---zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip \
---region=eu-central-1 \
---function-name=TweetsCounter \
+--zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip
+--region=eu-central-1
+--function-name=TweetsCounter
 --environment Variables="{CONSUMER_KEY=${CONSUMER_KEY}, CONSUMER_SECRET=${CONSUMER_SECRET}, ACCESS_TOKEN=${ACCESS_TOKEN}, ACCESS_TOKEN_SECRET=${ACCESS_TOKEN_SECRET}}"
 
 
