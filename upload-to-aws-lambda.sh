5f8fd6fe36975acb603c879b8d30c54e264786c7
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-05-11 12:05:53 +0200

    Syntax changed

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -3,5 +3,5 @@ aws lambda update-function-code \
 --zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
 --function-name=TweetsCounter \
---environment "Variables={CONSUMER_KEY=${CONSUMER_KEY}, CONSUMER_SECRET=${CONSUMER_SECRET}, ACCESS_TOKEN=${ACCESS_TOKEN}, ACCESS_TOKEN_SECRET=${ACCESS_TOKEN_SECRET}}"
+--environment '{"Variables":{"CONSUMER_KEY":${CONSUMER_KEY}, "CONSUMER_SECRET":${CONSUMER_SECRET}, "ACCESS_TOKEN":${ACCESS_TOKEN}, "ACCESS_TOKEN_SECRET":${ACCESS_TOKEN_SECRET}}}'
 
