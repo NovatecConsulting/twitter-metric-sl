7f1fae23b2ccbbc18c6d371964c285daf1baf281
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-05-11 10:34:44 +0200

    Add Envirmoment Variables to AWS too

--- upload-to-aws-lambda.sh
+++ upload-to-aws-lambda.sh
@@ -3,4 +3,6 @@ aws lambda update-function-code \
 --zip-file=fileb://build/distributions/twitter-metric-sl-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
 --function-name=TweetsCounter
+--environment Variables="{CONSUMER_KEY=$CONSUMER_KEY, CONSUMER_SECRET=$CONSUMER_SECRET, ACCESS_TOKEN=$ACCESS_TOKEN, ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET}"
+
 
